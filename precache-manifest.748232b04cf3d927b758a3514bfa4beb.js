2eb683d4c24863ae0c31f45fd1d8fad782754239
yehia ismael <yoyo200_868@hotmail.com>
2021-06-02 14:47:20 +0200

    Updates

---
+++ precache-manifest.748232b04cf3d927b758a3514bfa4beb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6eccf5690e6de1c54cfb",
+    "url": "/ya-social/static/css/main.382cbb1d.chunk.css"
+  },
+  {
+    "revision": "6eccf5690e6de1c54cfb",
+    "url": "/ya-social/static/js/main.6eccf569.chunk.js"
+  },
+  {
+    "revision": "738a564ccade05657848",
+    "url": "/ya-social/static/css/1.97ce977b.chunk.css"
+  },
+  {
+    "revision": "738a564ccade05657848",
+    "url": "/ya-social/static/js/1.738a564c.chunk.js"
+  },
+  {
+    "revision": "9c198343142a04afb492",
+    "url": "/ya-social/static/js/runtime~main.9c198343.js"
+  },
+  {
+    "revision": "998fa6aacd6f5a4b2709b0dc1effbdb6",
+    "url": "/ya-social/index.html"
+  }
+];
